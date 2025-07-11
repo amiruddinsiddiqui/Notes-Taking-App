df32529ca959256b4fa26debfee6a1ca412a174b
Amiruddin <amirvsidd@gmail.com>
2025-07-12 02:27:32 +0530

    connect to mongodb atlas replica set

--- src/main/java/com/amir/notestakingapp/controller/UserController.java
+++ src/main/java/com/amir/notestakingapp/controller/UserController.java
@@ -3,6 +3,9 @@ package com.amir.notestakingapp.controller;
 import com.amir.notestakingapp.entity.User;
 import com.amir.notestakingapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,17 +43,18 @@ public class UserController {
 
     @PutMapping("{userName}")
     @Transactional
-    public User updateUser(@RequestBody User user, @PathVariable String userName){
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName){
         try {
             User userInDB = userService.findByUserName(userName);
-            if (userInDB != null){
-                userInDB.setUserName(user.getUserName());
-                userInDB.setPassword(user.getPassword());
+            if (userInDB == null){
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
+            userInDB.setUserName(user.getUserName());
+            userInDB.setPassword(user.getPassword());
             userService.save(userInDB);
-            return userInDB;
+            return new ResponseEntity<>(userInDB, HttpStatus.OK);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to update",e);
         }
     }
 }
